fab23f23c6140b2c8f75d7eae88c2fefbc592387
BenjaminAlexander <alexan28@illinois.edu>
2018-10-21 16:44:18 -0700

    Adding a simple metaserver

---
+++ Game/Metaserver/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Metaserver
+{
+    class Program
+    {
+        private const int LISTEN_PORT = 3001;
+        private static TcpListener prelimListener;
+
+        static void Main(string[] args)
+        {
+            string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            Console.WriteLine("BulletHell metaserver is up Version: " + version);
+            prelimListener = new TcpListener(IPAddress.Any, LISTEN_PORT);
+            prelimListener.Start();
+            while (true)
+            {
+                TcpClient prelimClient = prelimListener.AcceptTcpClient();
+                Console.WriteLine(prelimClient.Client.RemoteEndPoint.ToString());
+                prelimClient.Close();
+            }
+            Console.WriteLine("BulletHell metaserver is down");
+        }
+    }
+}
