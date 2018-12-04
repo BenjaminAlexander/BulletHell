cede7ef7090843c67af555506daf5e0c31af5419
BenjaminAlexander <benalexander71792@gmail.com>
2018-12-03 21:05:40 -0800

    Debugging CI/CD pipeline

--- Automation/stopExpiredInstances.py
+++ Automation/stopExpiredInstances.py
@@ -1,10 +1,10 @@
 import awsUtils
 import boto3
 
-iam = boto3.resource('iam')
-current_user = iam.CurrentUser()
+#iam = boto3.resource('iam')
+#current_user = iam.CurrentUser()
 
-print(current_user.user_name)
+#print(current_user.user_name)
 
 ec2 = boto3.resource('ec2')
 instances = ec2.instances.all()
