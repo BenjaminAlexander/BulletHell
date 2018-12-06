c7adc28c4abb236a4d400f0a576dcf99b310f1fa
BenjaminAlexander <benalexander71792@gmail.com>
2018-12-05 21:29:01 -0800

    Adding a script to lanuch an EC2 instance with a Go Agent

--- Automation/launchGoAgentInstance.py
+++ Automation/launchGoAgentInstance.py
@@ -2,7 +2,4 @@ import awsUtils
 import boto3
 
 ec2 = boto3.resource('ec2')
-
-#testInstance = ec2.Instance('i-058f49164458489ae')
-#awsUtils.startInstanceWithStopTime(testInstance, 10)
 instance = awsUtils.launchAutoregisterGoAgentWindows(ec2, 720)
