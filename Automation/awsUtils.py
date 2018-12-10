12d5ee5e44f22b9fab10ded1f75fef379b0b9318
BenjaminAlexander <benalexander71792@gmail.com>
2018-12-10 15:14:54 -0800

    Adding a key par name to the EC2 instance

--- Automation/awsUtils.py
+++ Automation/awsUtils.py
@@ -48,6 +48,7 @@ def createMetaserverEC2Instance(ec2, deltaMinutes):
 def launchAutoregisterGoAgentWindows(ec2, deltaMinutes):
     stopTime = datetime.now(tz=tzutc()) + timedelta(minutes=deltaMinutes)
     newInstances = ec2.create_instances(ImageId='ami-0e1f2bf18c3831ba4',
+                                        KeyName='MyFirstKey.pem',
                                         InstanceType='t2.micro',
                                         MaxCount=1,
                                         MinCount=1,
